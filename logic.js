6f5c953f632506c8d9d9e50289dbd5db215c31df
Koshoo <benkosh@gmail.com>
2019-06-30 02:31:40 +0300

    working timer

--- logic.js
+++ logic.js
@@ -1,108 +1,93 @@
 const timer_ele = document.querySelector("#timer");
 const session_ele = document.querySelector("#session-time");
 const break_ele = document.querySelector("#break-time");
-const start_btn = document.querySelector("start-btn");
 
-const reset = document.querySelector("reset-btn");
-const pause = document.querySelector("pause-btn");
-const stop = document.querySelector("stop-btn");
+const start_btn = document.querySelector("#start-btn");
+const reset_btn = document.querySelector("#reset-btn");
+const pause_btn = document.querySelector("#pause-btn");
+const stop_btn = document.querySelector("#stop-btn");
 
 //Buttons array
 const buttons = document.querySelectorAll(".btn");
 
 let isOn = false;
-
-let session_time = 25;
+let session_time = String(25);
 let break_time = 5;
-session_ele.textContent = session_time + ":00";
-break_ele.textContent = break_time + ":00";
-timer.textContent = session_time + ":00";
+updateAll();
 
+buttons.forEach((btn) => btn.addEventListener("click", () => buttonHandler(btn.id)));
 
-buttons.forEach(function (button) {
-	button.addEventListener("click", function () {
-		buttonHandler(button.id)
-	})
-});
+function buttonHandler(id) {
+	session_time = Number(session_time);
+	if (id == "left-inc-btn") {
+		if (session_time < 60) {
+			session_time += 1;
+		}
+		updateAll();
 
-function buttonHandler(button) {
-	console.log(button)
+	} else if (id == "left-dec-btn") {
+		if (session_time > 1) {
+			session_time -= 1;
+		}
+		updateAll();
 
-	switch (button) {
+	} else if (id == "right-inc-btn") {
+		if (break_time < 20) {
+			break_time += 1;
+		}
+		updateAll();
 
-		case "left-inc-btn":
-			if (session_time < 60) {
-				session_time += 1;
-			}
-			break
-		case "left-dec-btn":
-			if (session_time > 1) {
-				session_time -= 1;
-			}
-			break
-
-		case "right-inc-btn":
-			if (break_time < 20) {
-				break_time += 1;
-			}
-			break
-		case "right-dec-btn":
-			if (break_time > 1) {
-				break_time -= 1;
-			}
-			break
-
-		case "start-btn":
-			isOn = true;
-
-			var current_session_time = session_time;
-			break
-
-		case "reset-btn":
-			isOn = false;
-			session_time = 25;
-			break_time = 5;
-			break
-
-		case "stop-btn":
-			isOn = false;
-			current_session_time = current_session_time;
-			break;
-
+	} else if (id == "right-dec-btn") {
+		if (break_time > 1) {
+			break_time -= 1;
+		}
+		updateAll();
+	} else if (id == "start-btn") {
+		if (!isOn) {
+			startClock()
+		}
+	} else if (id == "reset-btn") {
+		isOn = false;
+		session_time = String(25);
+		break_time = 5;
+		updateAll();
 	}
 
-	session_ele.textContent = session_time + ":00";
-	break_ele.textContent = break_time + ":00";
+}
+
+function updateAll() {
+	session_ele.textContent = session_time
+	break_ele.textContent = break_time
 	if (!isOn) {
-		timer.textContent = session_time + ":00";
-	} else {
-		timer_ele.style.color = "blue";
-		operateClock()
+		timer_ele.textContent = session_time + ":00";
 	}
 }
 
-function operateClock() {
-	let time = timer.textContent.split(":")
-	let minutes = parseInt(time[0]);
-	let seconds = time[1]
+function startClock() {
+	isOn = true;
+	session_time = String(session_time);
+	let minutes = Number(session_time.substring(0, 2)) - 1;
+	let seconds = 59;
+
+	reset_btn.addEventListener("click", () => clearInterval(intervalId));
 
 	let intervalId = setInterval(function () {
-			if (seconds == 0 && minutes > 0) {
-				seconds = 59;
-				minutes -= 1;
-			} else if (seconds > 0 && minutes > 0) {
-				seconds -= 1;
-			}
-		timer.textContent = minutes + ":" + seconds;
-		console.log(isOn)
-		if(!isOn){
-			clearInterval(intervalId)
+		if (seconds < 10) {
+			timer_ele.textContent = minutes + ":" + "0" + seconds;
+		} else {
+			timer_ele.textContent = minutes + ":" + seconds;
 		}
+		if (seconds > 0) {
+			seconds -= 1;
+		} else {
+			minutes -= 1;
+			seconds = 59;
+		}
+
+		if(minutes == 0 && seconds == 0){
+			alert("Times up");
+			clearInterval(intervalId);
+		}
+
 	}, 1000);
-
-	
-
-
-
-
 }
