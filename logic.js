7b815bbc445aa2eb0290cdaecba44f3e553fad3d
Koshoo <benkosh@gmail.com>
2019-06-30 04:12:17 +0300

    working alpha

--- logic.js
+++ logic.js
@@ -1,18 +1,19 @@
 const timer_ele = document.querySelector("#timer");
 const session_ele = document.querySelector("#session-time");
 const break_ele = document.querySelector("#break-time");
-
+const session_text = document.querySelector("#session");
+const break_text = document.querySelector("#break");
 const start_btn = document.querySelector("#start-btn");
 const reset_btn = document.querySelector("#reset-btn");
 const pause_btn = document.querySelector("#pause-btn");
 const stop_btn = document.querySelector("#stop-btn");
-
-//Buttons array
+//Buttons array for eventlisteners
 const buttons = document.querySelectorAll(".btn");
-
 let isOn = false;
 let session_time = String(25);
 let break_time = 5;
+let isPause = false;
+
 updateAll();
 
 buttons.forEach((btn) => btn.addEventListener("click", () => buttonHandler(btn.id)));
@@ -46,6 +47,8 @@ function buttonHandler(id) {
 		if (!isOn) {
 			startClock()
 		}
+		isPause = false;
+		timer_ele.style.color = "rgb(35, 226, 35)"
 	} else if (id == "reset-btn") {
 		isOn = false;
 		session_time = String(25);
@@ -65,29 +68,126 @@ function updateAll() {
 
 function startClock() {
 	isOn = true;
+	timer_ele.style.color = "rgb(35, 226, 35)"
+	session_text.style.color = "rgb(35, 226, 35)"
 	session_time = String(session_time);
 	let minutes = Number(session_time.substring(0, 2)) - 1;
 	let seconds = 59;
 
-	reset_btn.addEventListener("click", () => clearInterval(intervalId));
 
 	let intervalId = setInterval(function () {
-		if (seconds < 10) {
-			timer_ele.textContent = minutes + ":" + "0" + seconds;
-		} else {
-			timer_ele.textContent = minutes + ":" + seconds;
-		}
-		if (seconds > 0) {
-			seconds -= 1;
-		} else {
-			minutes -= 1;
-			seconds = 59;
-		}
+		if (!isPause) {
+			if (seconds < 10) {
+				timer_ele.textContent = minutes + ":" + "0" + seconds;
+			} else {
+				timer_ele.textContent = minutes + ":" + seconds;
+			}
+			if (seconds > 0) {
+				seconds -= 1;
+			} else {
+				minutes -= 1;
+				seconds = 59;
+			}
 
-		if(minutes == 0 && seconds == 0){
-			alert("Times up");
-			clearInterval(intervalId);
+			if (minutes == 0 && seconds == 0) {
+				breakTime();
+				session_text.style.color = "black"
+				clearInterval(intervalId);
+			}
 		}
-
 	}, 1000);
+
+	//buttons
+	reset_btn.addEventListener("click", () => {
+		clearInterval(intervalId)
+		timer_ele.style.color = "black"
+		session_text.style.color = "black"
+		isOn = false;
+		isPause = false;
+	});
+
+	pause_btn.addEventListener("click", () => {
+		isPause = true;
+		timer_ele.style.color = "red"
+	});
+
+	stop_btn.addEventListener("click", () => {
+		clearInterval(intervalId)
+		timer_ele.style.color = "black"
+		isOn = false;
+		isPause = false;
+		session_ele.textContent = session_time;
+		break_ele.textContent = break_time;
+		timer_ele.textContent = session_time + ":00";
+		timer_ele.style.color = "black";
+		break_ele.style.color = "black";
+		session_ele.style.color = "black";
+
+	});
+
+}
+
+
+
+
+function breakTime() {
+
+	isOn = true;
+	timer_ele.style.color = "rgb(35, 226, 35)";
+	break_text.style.color = "rgb(35, 226, 35)";
+	let minutes = Number(break_time) - 1;
+	let seconds = 59;
+
+
+
+	let intervalId = setInterval(function () {
+		if (!isPause) {
+			if (seconds < 10) {
+				timer_ele.textContent = minutes + ":" + "0" + seconds;
+			} else {
+				timer_ele.textContent = minutes + ":" + seconds;
+			}
+			if (seconds > 0) {
+				seconds -= 1;
+			} else {
+				minutes -= 1;
+				seconds = 59;
+			}
+
+			if (minutes == 0 && seconds == 0) {
+				clearInterval(intervalId);
+				break_text.style.color = "black";
+				startClock();
+			}
+		}
+	}, 1000);
+
+	//buttons
+	reset_btn.addEventListener("click", () => {
+		clearInterval(intervalId)
+		timer_ele.style.color = "black";
+		isOn = false;
+		isPause = false;
+	});
+
+	pause_btn.addEventListener("click", () => {
+		isPause = true;
+		timer_ele.style.color = "red";
+
+	});
+
+	stop_btn.addEventListener("click", () => {
+		clearInterval(intervalId)
+		timer_ele.style.color = "black";
+		isOn = false;
+		isPause = false;
+		session_ele.textContent = session_time;
+		break_ele.textContent = break_time;
+		timer_ele.textContent = session_time + ":00";
+		timer_ele.style.color = "black";
+		break_ele.style.color = "black";
+		session_ele.style.color = "black";
+		s
+	});
+
 }
